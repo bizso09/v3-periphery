2a620d5889d60951070b5d51a3cb020b6a0f0443
bizso09 <bizso09@users.noreply.github.com>
2023-08-04 10:20:07 +0400

    Update INonfungiblePositionManager.sol

--- contracts/interfaces/INonfungiblePositionManager.sol
+++ contracts/interfaces/INonfungiblePositionManager.sol
@@ -76,6 +76,26 @@ interface INonfungiblePositionManager is
             uint128 tokensOwed1
         );
 
+    // details about the uniswap position
+    struct Position {
+        // the nonce for permits
+        uint96 nonce;
+        // the address that is approved for spending this token
+        address operator;
+        // the ID of the pool with which this token is connected
+        uint80 poolId;
+        // the tick range of the position
+        int24 tickLower;
+        int24 tickUpper;
+        // the liquidity of the position
+        uint128 liquidity;
+        // the fee growth of the aggregate position as of the last action on the individual position
+        uint256 feeGrowthInside0LastX128;
+        uint256 feeGrowthInside1LastX128;
+        // how many uncollected tokens are owed to the position, as of the last computation
+        uint128 tokensOwed0;
+        uint128 tokensOwed1;
+    }
     struct MintParams {
         address token0;
         address token1;
